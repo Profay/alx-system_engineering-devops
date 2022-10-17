e09b1647dadf06a1902e2e890fa49d194571aeb7
PROFAY <Oyewaletimmy01@gmail.com>
2022-10-17 12:48:58 +0000

    api list

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,6 +7,7 @@ import json
 import requests
 import sys
 
+
 def get_user_todo_list():
     employee_id = sys.argv[1]
     url1 = 'https://jsonplaceholder.typicode.com/users/%s' % employee_id
@@ -15,12 +16,13 @@ def get_user_todo_list():
     user = requests.get(url1).json()
     completed_todo = []
     for todo in todo_list:
-        if todo.get('completed') == True:
+        if todo.get('completed') is True:
             completed_todo.append(todo.get('title'))
 
-    print("First line: Employee {} is done with tasks({}/{}): \n"
-    .format(user.get('name'), len(completed_todo), len(todo_list)))
-    
+    print("Employee {} is done with tasks({}/{}): "
+          .format(user.get('name'), len(completed_todo), len(todo_list)))
+    for todo in completed_todo:
+        print("\t {}".format(todo))
 
 
 if __name__ == '__main__':
